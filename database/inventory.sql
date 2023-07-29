100a22eeb981d278c388178fe9e39ffeecce600c
Fauzian890 <fauzian.achmad123@gmail.com>
2023-07-29 11:55:08 +0700

    db new

--- database/inventory.sql
+++ database/inventory.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2022 at 07:19 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: Jul 29, 2023 at 06:54 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `biosysdb`
+-- Database: `inventory`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barangkeluar`
+--
+
+CREATE TABLE `barangkeluar` (
+  `id_barangkeluar` int(11) NOT NULL,
+  `id_kopi` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_konsumen` int(11) NOT NULL,
+  `tanggal_barangkeluar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barangmasuk`
+--
+
+CREATE TABLE `barangmasuk` (
+  `id_barangmasuk` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_kopi` int(11) NOT NULL,
+  `id_supplier` int(11) NOT NULL,
+  `tanggal_barangmasuk` date NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,17 +82,75 @@ CREATE TABLE `clients` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(3, 'bubuk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konsumen`
+--
+
+CREATE TABLE `konsumen` (
+  `id_konsumen` int(11) NOT NULL,
+  `nama_konsumen` varchar(25) NOT NULL,
+  `alamat_konsumen` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kopi`
+--
+
+CREATE TABLE `kopi` (
+  `id_kopi` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `namakopi` varchar(25) NOT NULL,
+  `deskripsi` varchar(25) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
   `ProdId` int(10) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `Namakopi` varchar(25) NOT NULL,
   `ProdDescription` varchar(50) NOT NULL,
   `ProdQuantity` int(10) NOT NULL,
   `ProdUnit` varchar(50) NOT NULL,
-  `ProdUnitPrice` double NOT NULL,
-  `ProdExpiry` date NOT NULL
+  `ProdUnitPrice` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProdId`, `id_kategori`, `Namakopi`, `ProdDescription`, `ProdQuantity`, `ProdUnit`, `ProdUnitPrice`) VALUES
+(3, 0, '', 'qww', 1, '2', 12),
+(4, 0, '', 'asd', 2, '32', 13),
+(5, 0, '', 'sad', 2, '4', 12),
+(6, 0, '', 'kopi asu', 10, '90', 100),
+(7, 0, '', 'kopi asu', 78, '8', 30000),
+(8, 0, 'asu', 'kopi asu', 2, '3', 20000);
 
 -- --------------------------------------------------------
 
@@ -89,9 +176,42 @@ CREATE TABLE `purchaseorder` (
 --
 
 CREATE TABLE `supplier` (
+  `id_supplier` int(11) NOT NULL,
+  `nama_supplier` varchar(25) NOT NULL,
+  `alamat_supplier` varchar(25) NOT NULL,
+  `no_hp` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
   `SupId` int(11) NOT NULL,
   `SupName` varchar(50) NOT NULL,
-  `SupAddress` varchar(50) NOT NULL
+  `SupAddress` varchar(50) NOT NULL,
+  `nohp` int(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`SupId`, `SupName`, `SupAddress`, `nohp`) VALUES
+(2, 'sdasd', 'sadasd', 324234);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -118,6 +238,24 @@ INSERT INTO `users` (`UserID`, `UserName`, `UserPassword`) VALUES
 --
 
 --
+-- Indexes for table `barangkeluar`
+--
+ALTER TABLE `barangkeluar`
+  ADD PRIMARY KEY (`id_barangkeluar`),
+  ADD KEY `id_kopi` (`id_kopi`),
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_konsumen` (`id_konsumen`);
+
+--
+-- Indexes for table `barangmasuk`
+--
+ALTER TABLE `barangmasuk`
+  ADD PRIMARY KEY (`id_barangmasuk`),
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_kopi` (`id_kopi`),
+  ADD KEY `id_supplier` (`id_supplier`);
+
+--
 -- Indexes for table `clientorder`
 --
 ALTER TABLE `clientorder`
@@ -130,10 +268,30 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`ClientId`);
 
 --
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `konsumen`
+--
+ALTER TABLE `konsumen`
+  ADD PRIMARY KEY (`id_konsumen`);
+
+--
+-- Indexes for table `kopi`
+--
+ALTER TABLE `kopi`
+  ADD PRIMARY KEY (`id_kopi`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`ProdId`);
+  ADD PRIMARY KEY (`ProdId`),
+  ADD KEY `id_kategori` (`id_kategori`);
 
 --
 -- Indexes for table `purchaseorder`
@@ -145,7 +303,19 @@ ALTER TABLE `purchaseorder`
 -- Indexes for table `supplier`
 --
 ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
   ADD PRIMARY KEY (`SupId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- Indexes for table `users`
@@ -156,6 +326,18 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `barangkeluar`
+--
+ALTER TABLE `barangkeluar`
+  MODIFY `id_barangkeluar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `barangmasuk`
+--
+ALTER TABLE `barangmasuk`
+  MODIFY `id_barangmasuk` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `clientorder`
@@ -170,10 +352,28 @@ ALTER TABLE `clients`
   MODIFY `ClientId` int(10) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `konsumen`
+--
+ALTER TABLE `konsumen`
+  MODIFY `id_konsumen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kopi`
+--
+ALTER TABLE `kopi`
+  MODIFY `id_kopi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ProdId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ProdId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `purchaseorder`
@@ -185,13 +385,51 @@ ALTER TABLE `purchaseorder`
 -- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
-  MODIFY `SupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `SupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barangkeluar`
+--
+ALTER TABLE `barangkeluar`
+  ADD CONSTRAINT `bk_fk1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`),
+  ADD CONSTRAINT `bk_fk2` FOREIGN KEY (`id_konsumen`) REFERENCES `konsumen` (`id_konsumen`),
+  ADD CONSTRAINT `bk_fk3` FOREIGN KEY (`id_kopi`) REFERENCES `kopi` (`id_kopi`);
+
+--
+-- Constraints for table `barangmasuk`
+--
+ALTER TABLE `barangmasuk`
+  ADD CONSTRAINT `bm_fk1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`),
+  ADD CONSTRAINT `bm_fk2` FOREIGN KEY (`id_kopi`) REFERENCES `kopi` (`id_kopi`),
+  ADD CONSTRAINT `bm_fk3` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id_supplier`);
+
+--
+-- Constraints for table `kopi`
+--
+ALTER TABLE `kopi`
+  ADD CONSTRAINT `kopi_fk1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
